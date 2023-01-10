5e4f0b0ce7c058ce135a056e596f0f1608de79bb
123130 <58681779+123130@users.noreply.github.com>
2023-01-10 19:40:36 +0800

    Update pop.py

--- pop.py
+++ pop.py
@@ -2,16 +2,14 @@ from scapy.packet import *
 from scapy.fields import *
 from scapy.ansmachine import *
 from scapy.layers.inet import *
-import dissector
 
 
 class POPField(StrField):
     """
-    field class for handling pop requests
+    field class for handling pop requests or pop response
     @attention: it inherets StrField from Scapy library
     """
     holds_packets = 1
-    name = "POPField"
 
     def getfield(self, pkt, s):
         """
@@ -23,28 +21,31 @@ class POPField(StrField):
         @param pkt: holds the whole packet
         @param s: holds only the remaining data which is not dissected yet.
         """
-        cstream = -1
-        if pkt.underlayer.name == "TCP":
-            cstream = dissector.check_stream(\
-            pkt.underlayer.underlayer.fields["src"],\
-             pkt.underlayer.underlayer.fields["dst"],\
-              pkt.underlayer.fields["sport"],\
-               pkt.underlayer.fields["dport"],\
-                pkt.underlayer.fields["seq"], s)
-        if not cstream == -1:
-            s = cstream
         remain = ""
         value = ""
         ls = s.splitlines()
-        myresult = []
-        lslen = len(ls)
+        length_ls = len(ls)
+        ls01 = []
+        for i in range(length_ls):
+            str01 = str(ls[i], encoding="utf-8")
+            if i == 0 and len(str01) > 3:
+                str_list = str01.split('-')
+                for j in range(len(str_list)):
+                    if str_list[j] == '':
+                        continue
+                    else:
+                        ls01.append(str_list[j])
+            else:
+                ls01.append(str01)
+        myresult = ''
+        lslen = len(ls01)
         i = 0
         k = 0
-        for line in ls:
+        for line in ls01:
             k = k + 1
             ls2 = line.split()
             length = len(ls2)
-            if length > 1:
+            if length > 1 and k == 1:
                 value = ls2[0]
                 c = 1
                 remain = ""
@@ -52,15 +53,20 @@ class POPField(StrField):
                     remain = remain + ls2[c] + " "
                     c = c + 1
                 if self.name.startswith("request"):
-                    myresult = myresult + "Request Command: " + value +\
-                    ", Request Parameter(s): " + remain
-                    if k < lslen:
-                        myresult = myresult + " | "
+                    myresult = myresult + "Request command: " + value +\
+                    ", Request parameter: " + remain
                 if self.name.startswith("response"):
-                    myresult = myresult + "Response Indicator: " + value +\
-                    ", Response Parameter(s): " + remain
-                    if k < lslen:
-                        myresult = myresult + " | "
+                    myresult = myresult + "Response indicator: " + value +\
+                    ", Response description: " + remain
+            if k > 1:
+                remain = str(ls2[0])
+                c = 1
+                while c < length:
+                    remain = remain + " " + str(ls2[c])
+                    c = c + 1
+                myresult = myresult + remain
+            if length == 1 and length_ls == 1:
+                myresult = "Request command: " + str(ls2[0])
             i = i + 1
             if i == lslen:
                 return "", myresult
@@ -75,8 +81,8 @@ class POPField(StrField):
         @param remain: this parameter specifies the size of the remaining
         data so make it 0 to handle all of the data.
         """
+        super().__init__(name, default, fmt)
         self.name = name
-        StrField.__init__(self, name, default, fmt, remain)
 
 
 class POPRes(Packet):
